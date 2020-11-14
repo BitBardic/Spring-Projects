8803697609c74407080ac0a5053a8eb9e48f7692
Nasir Iqbal <nasirpucit@hotmail.com>
2020-11-14 14:19:06 +0000

    setup singleton scope through java config

--- conference/src/main/java/AppConfig.java
+++ conference/src/main/java/AppConfig.java
@@ -2,13 +2,16 @@ import com.codewithnas.repository.HibernateSpeakerRepositoryImpl;
 import com.codewithnas.repository.SpeakerRepository;
 import com.codewithnas.service.SpeakerService;
 import com.codewithnas.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value= BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());
