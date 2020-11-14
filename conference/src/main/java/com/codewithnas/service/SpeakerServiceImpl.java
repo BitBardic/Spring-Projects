25dde1dfe4b77fc311934bc296c940e7d011449c
Nasir Iqbal <nasirpucit@hotmail.com>
2020-11-14 14:43:30 +0000

    Demo: Autowired

--- conference/src/main/java/com/codewithnas/service/SpeakerServiceImpl.java
+++ conference/src/main/java/com/codewithnas/service/SpeakerServiceImpl.java
@@ -3,6 +3,7 @@ package com.codewithnas.service;
 import com.codewithnas.model.Speaker;
 import com.codewithnas.repository.HibernateSpeakerRepositoryImpl;
 import com.codewithnas.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -10,13 +11,20 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
     public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
-    /*public void setRepository(SpeakerRepository repository) {
+    @Autowired
+    public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
-    }*/
+    }
 
     public List<Speaker> findAll() {
         return repository.findAll();
