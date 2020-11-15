b0c0dfa81a9ce04f2f2002527f0f99d310b9ad1e
Nasir Iqbal <nasirpucit@hotmail.com>
2020-11-15 00:37:39 +0000

    added example of Bean Profiles

--- conference/src/main/java/com/codewithnas/service/SpeakerServiceImpl.java
+++ conference/src/main/java/com/codewithnas/service/SpeakerServiceImpl.java
@@ -4,12 +4,14 @@ import com.codewithnas.model.Speaker;
 import com.codewithnas.repository.HibernateSpeakerRepositoryImpl;
 import com.codewithnas.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
+@Profile("dev")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
