57edb6738f98d1dfedb330a165ccc3032de90a98
Nasir Iqbal <nasirpucit@hotmail.com>
2020-11-15 00:18:20 +0000

    added FactoryBean sample

--- conference/src/main/java/AppConfig.java
+++ conference/src/main/java/AppConfig.java
@@ -2,16 +2,30 @@ import com.codewithnas.repository.HibernateSpeakerRepositoryImpl;
 import com.codewithnas.repository.SpeakerRepository;
 import com.codewithnas.service.SpeakerService;
 import com.codewithnas.service.SpeakerServiceImpl;
+import com.codewithnas.util.CalendarFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Calendar;
+
 @Configuration
 @ComponentScan({"com.codewithnas"})
 public class AppConfig {
 
+    @Bean(name = "cal")
+    public CalendarFactory calFactory() {
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar cal() throws Exception {
+        return calFactory().getObject();
+    }
     /*
     @Bean(name = "speakerService")
     @Scope(value= BeanDefinition.SCOPE_SINGLETON)
